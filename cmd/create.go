31b76c6e2e01a4f1a9f46bec85a528c366ed2e61
AriSu2904 <arisusanto290401@gmail.com>
2025-09-27 11:12:02 +0700

    feat(gong): add command create for parent command

---
+++ cmd/create.go
@@ -0,0 +1,25 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"log"
+)
+
+var createCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Main command to create a template or spesific module",
+	Long:  `Main command to create a template or spesific module`,
+	Run: func(cmd *cobra.Command, args []string) {
+		err := cmd.Help()
+		if err != nil {
+			log.Fatal(err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(createCmd)
+}
